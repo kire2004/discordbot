daf26c9d254428ae624bb1198ec625512b195f29
Erik Rackley <erik629@gmail.com>
2017-07-28 22:30:14 -0700

    I created an object to store my token from Discord.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = require('./token.js');
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -11,4 +12,4 @@ client.on('message', message => {
   }
 });
 
-client.login('your token');
+client.login();
---
+++ token.js
@@ -0,0 +1,4 @@
+module.exports =
+{
+    token: 'YOUR_TOKEN_HERE'
+}
